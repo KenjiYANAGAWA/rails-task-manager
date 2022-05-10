c80b9d8c8012b7d17e480ca9dc93161acffe1906
Kenji Yanagawa <kenchibi@gmail.com>
2022-05-10 16:59:39 +0900

    update not delete yet

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,6 +18,14 @@ class TasksController < ApplicationController
     @task.save
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+  end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   get '/task/:id', to: 'tasks#show', as: :task
   get '/tasks/new', to: 'tasks#new', as: :new_task
   post '/tasks', to: 'tasks#create'
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch '/task/:id', to: 'tasks#update'
 end
