48aa6c0325cbe24788eadca33cebe7005b7d1f56
Kenji Yanagawa <kenchibi@gmail.com>
2022-05-10 21:37:06 +0900

    directory always plural

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,12 @@ Rails.application.routes.draw do
   # get '/tasks', to: 'tasks#index', as: :tasks
   # get '/tasks/new', to: 'tasks#new', as: :new_task
 
-  # get '/task/:id', to: 'tasks#show', as: :task
+  # get '/tasks/:id', to: 'tasks#show', as: :task
   # post '/tasks', to: 'tasks#create'
 
   # get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
-  # patch '/task/:id', to: 'tasks#update'
+  # patch '/tasks/:id', to: 'tasks#update'
 
-  # delete '/task/:id', to: 'tasks#destroy'
+  # delete '/tasks/:id', to: 'tasks#destroy'
   resources :tasks
 end
